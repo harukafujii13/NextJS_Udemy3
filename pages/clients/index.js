502e85d016b6fdcc593236cd8b051a9e6e92c129
Haruka <hfujii1013@gmail.com>
2023-06-05 00:35:25 -0700

    section3-65 done

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,19 @@
+import Link from "next/link";
 function ClientPage() {
+  const clients = [
+    { id: "haruka", name: "Haruka" },
+    { id: "max", name: "max" },
+  ];
   return (
     <div>
       <h1>The Client Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}> {client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
